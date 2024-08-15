641bb971e74c12d06d0b3df94e310479a38cddba
Aditya kumar <148646479+K-Aditya1210@users.noreply.github.com>
2024-08-15 21:45:05 +0530

    Create SudokuSolver.java

---
+++ Prodigy_SD_04/SudokuSolver.java
@@ -0,0 +1,93 @@
+public class SudokuSolver {
+
+    private static final int GRID_SIZE = 9;
+
+    public static void main(String[] args) {
+        // Example unsolved Sudoku puzzle (0 represents empty cells)
+        int[][] Sudokuboard = {
+                {5, 3, 0, 0, 7, 0, 0, 0, 0},
+                {6, 0, 0, 1, 9, 5, 0, 0, 0},
+                {0, 9, 8, 0, 0, 0, 0, 6, 0},
+                {8, 0, 0, 0, 6, 0, 0, 0, 3},
+                {4, 0, 0, 8, 0, 3, 0, 0, 1},
+                {7, 0, 0, 0, 2, 0, 0, 0, 6},
+                {0, 6, 0, 0, 0, 0, 2, 8, 0},
+                {0, 0, 0, 4, 1, 9, 0, 0, 5},
+                {0, 0, 0, 0, 8, 0, 0, 7, 9}
+        };
+
+        if (solveBoard(Sudokuboard)) {
+            System.out.println("Sudoku solved successfully!");
+            printBoard(Sudokuboard);
+        } else {
+            System.out.println("This Sudoku puzzle cannot be solved.");
+        }
+    }
+
+    public static boolean solveBoard(int[][] board) {
+        for (int row = 0; row < GRID_SIZE; row++) {
+            for (int col = 0; col < GRID_SIZE; col++) {
+                if (board[row][col] == 0) {
+                    for (int num = 1; num <= 9; num++) {
+                        if (isValid(board, num, row, col)) {
+                            board[row][col] = num;
+
+                            if (solveBoard(board)) {
+                                return true;
+                            } else {
+                                board[row][col] = 0;
+                            }
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public static boolean isValid(int[][] board, int num, int row, int col) {
+        // Check if the number is already in the row
+        for (int i = 0; i < GRID_SIZE; i++) {
+            if (board[row][i] == num) {
+                return false;
+            }
+        }
+
+        // Check if the number is already in the column
+        for (int i = 0; i < GRID_SIZE; i++) {
+            if (board[i][col] == num) {
+                return false;
+            }
+        }
+
+        // Check if the number is in the 3x3 sub-grid
+        int startRow = row - row % 3;
+        int startCol = col - col % 3;
+
+        for (int i = startRow; i < startRow + 3; i++) {
+            for (int j = startCol; j < startCol + 3; j++) {
+                if (board[i][j] == num) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    public static void printBoard(int[][] board) {
+        for (int row = 0; row < GRID_SIZE; row++) {
+            if (row % 3 == 0 && row != 0) {
+                System.out.println("-----------");
+            }
+            for (int col = 0; col < GRID_SIZE; col++) {
+                if (col % 3 == 0 && col != 0) {
+                    System.out.print("|");
+                }
+                System.out.print(board[row][col]);
+            }
+            System.out.println();
+        }
+    }
+}
